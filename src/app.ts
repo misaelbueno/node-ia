0ab2806c0dfbcec31fe545e44ac8cb43a3845893
misael bueno <misa.hbueno@gmail.com>
2026-07-04 11:44:56 -0300

    mudando formato da saida estruturada dos dados

--- src/app.ts
+++ src/app.ts
@@ -2,46 +2,52 @@ import dotenv from "dotenv";
 import express from "express";
 import { OpenAI } from "openai";
 import z from "zod";
+import { zodResponseFormat } from "openai/helpers/zod.mjs";
 
 dotenv.config();
-
-const app = express();
-app.use(express.json());
 
 const client = new OpenAI({
   apiKey: process.env.OPEN_AI_API_KEY,
 });
 
+const app = express();
+app.use(express.json());
+
+const schema = z.object({
+  produtos: z.array(z.string()),
+});
+
 app.post("/generate", async (req, res) => {
-  const completion = await client.chat.completions.create({
-    model: "gpt-4o-mini",
-    max_completion_tokens: 100,
-    // JSON_MODE
-    response_format: { type: "json_object" },
-    messages: [
-      {
-        role: "developer", // definir uma regra
-        content:
-          "Liste 3 produtos que atendam a necessidade do usuário. Responda em JSON no formato { produto: [string] }",
-      },
-      {
-        role: "user",
-        content: req.body.message,
-      },
-    ],
-  });
+  try {
+    const completion = await client.chat.completions.parse({
+      model: "gpt-4o-mini",
+      max_completion_tokens: 100,
+      response_format: zodResponseFormat(schema, "produtos_schema"),
+      messages: [
+        {
+          role: "developer", // definir uma regra
+          content:
+            "Liste 3 produtos que atendam a necessidade do usuário. Responda em JSON no formato { produto: [string] }",
+        },
+        {
+          role: "user",
+          content: req.body.message,
+        },
+      ],
+    });
 
-  const output = JSON.parse(completion.choices[0].message.content ?? "");
-  const schema = z.object({
-    produto: z.array(z.string()),
-  });
+    if (!completion.choices[0].message.refusal) {
+      res
+        .status(400)
+        .json({ error: "The model refused to generate a response." });
+      return;
+    }
 
-  const result = schema.safeParse(output);
-  if (!result.success) {
-    return res.status(400).json({ error: "Invalid output format" });
+    res.json(completion.choices[0].message.parsed?.produtos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-
-  res.json(output);
 });
 
 export default app;
